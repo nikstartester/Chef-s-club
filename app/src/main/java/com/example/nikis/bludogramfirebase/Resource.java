f9647b83a3ef24e148392c1f50ede2289eadbc26
nikistartester <nikistar986@gmail.com>
2018-10-13 10:33:04 +0300

    Now Parcelable

--- app/src/main/java/com/example/nikis/bludogramfirebase/Resource.java
+++ app/src/main/java/com/example/nikis/bludogramfirebase/Resource.java
@@ -1,9 +1,11 @@
 package com.example.nikis.bludogramfirebase;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-public class Resource<T> {
+public class Resource<T extends BaseData> implements Parcelable {
     @NonNull
     public final Status status;
 
@@ -29,21 +31,54 @@ public class Resource<T> {
         this.exception = exception;
     }
 
-    public static <T> Resource<T> success(@NonNull T data) {
+    public static <T extends BaseData> Resource<T> success(@NonNull T data) {
         return new Resource<>(Status.SUCCESS, data, null, null);
     }
 
-    public static <T> Resource<T> error(Exception e, String msg, @Nullable T data) {
+    public static <T extends BaseData> Resource<T> error(Exception e, String msg, @Nullable T data) {
         return new Resource<>(Status.ERROR, data, msg, e);
     }
 
-    public static <T> Resource<T> error(Exception e, @Nullable T data) {
+    public static <T extends BaseData> Resource<T> error(Exception e, @Nullable T data) {
         return new Resource<>(Status.ERROR, data, e);
     }
 
-    public static <T> Resource<T> loading(@Nullable T data) {
+    public static <T extends BaseData> Resource<T> loading(@Nullable T data) {
         return new Resource<>(Status.LOADING, data, null, null);
     }
 
     public enum Status { SUCCESS, ERROR, LOADING }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.status.ordinal());
+        dest.writeParcelable(this.data, flags);
+        dest.writeString(this.message);
+        dest.writeSerializable(this.exception);
+    }
+
+    protected Resource(Parcel in) {
+        int tmpStatus = in.readInt();
+        this.status = tmpStatus == -1 ? null : Status.values()[tmpStatus];
+        this.data = in.readParcelable(BaseData.class.getClassLoader());
+        this.message = in.readString();
+        this.exception = (Exception) in.readSerializable();
+    }
+
+    public static final Parcelable.Creator<Resource> CREATOR = new Parcelable.Creator<Resource>() {
+        @Override
+        public Resource createFromParcel(Parcel source) {
+            return new Resource(source);
+        }
+
+        @Override
+        public Resource[] newArray(int size) {
+            return new Resource[size];
+        }
+    };
 }
