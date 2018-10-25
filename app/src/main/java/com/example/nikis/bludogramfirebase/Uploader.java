82d500bb85c2c634031cf5b5d24a91c55a934eb3
nikistartester <nikistar986@gmail.com>
2018-10-25 23:34:48 +0300

    "T" rename to "Data". Add Data data (ext BaseData) var

--- app/src/main/java/com/example/nikis/bludogramfirebase/Uploader.java
+++ app/src/main/java/com/example/nikis/bludogramfirebase/Uploader.java
@@ -1,8 +1,32 @@
 package com.example.nikis.bludogramfirebase;
 
-public abstract class Uploader<T extends BaseData> {
-    public abstract void start(T data, OnProcessListener<T> onProcessListener);
-    
+import android.support.annotation.Nullable;
+
+public abstract class Uploader<Data extends BaseData> extends Object{
+
+    protected Data mData;
+
+    protected Resource<Data> mDataResource;
+
+    @Nullable
+    private OnProcessListener<Data> mOnProcessListener;
+
+    public  void start(Data data, OnProcessListener<Data> onProcessListener){
+        mData = data;
+        mOnProcessListener = onProcessListener;
+
+        start();
+    }
+
+    protected abstract void start();
+
+
+    protected void updateProgress(Resource<Data> resource){
+        if (mOnProcessListener != null) {
+            mOnProcessListener.onStatusChanged(resource);
+        }
+    }
+
     public interface OnProcessListener<Data extends BaseData>{
         public void onStatusChanged(Resource<Data> resource);
     }
